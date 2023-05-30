9a0fb87d6ee97c846ffe3ff6817f46b546348ed4
Jason Balamb <jasonst.106@gmail.com>
2023-05-29 21:43:40 -0500

    Dont now what I did with the button but is fucked up :(

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log('Hello, kittens lovers');
 
-const URL = 'https://api.thecatapi.com/v1/images/search?limit=10';
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3';
 
 // fetch(URL) //fetch returns a promise, and a promise is resolve with a '.then'
 //     .then(response => response.json()) //the answer we get with fetch, is transform in a javascript object
@@ -10,11 +10,17 @@ const URL = 'https://api.thecatapi.com/v1/images/search?limit=10';
 //     });
 
 async function myCat () {
-    const response = await fetch(URL);
+    const response = await fetch(API_URL);
     const data = await response.json();
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+
+    console.log(data)
+    const cat1 = document.getElementById('cat1');
+    const cat2 = document.getElementById('cat2');
+    const cat3 = document.getElementById('cat3');
+
+    cat1.src = data[0].url;
+    cat2.src = data[1].url;
+    cat3.src = data[2].url;
 }
 
-const myButton = document.querySelector('button');
-myButton.onclick = myCat;
+myCat();
